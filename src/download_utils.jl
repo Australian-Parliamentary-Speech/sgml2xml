95eeedc3c46cb8acccd9d35c383bde643611c4f1
Eve <evecheng999@gmail.com>
2025-11-03 16:01:38 +1100

    name

--- src/download_utils.jl
+++ src/download_utils.jl
@@ -2,7 +2,7 @@ using HTTP
 
 function get_response(url)
     # Parse and properly encode the URL to handle spaces and special characters
-    encoded_url = replace(url, r" +" => "") 
+    encoded_url = replace(url, r" +" => "%20") 
     response = HTTP.get(encoded_url, ["User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"])
     return response
 end
